b02ce859c77a56bfd2a451c1351225d24964392e
Salman Al-Ghamdi <salmanyghamdi@gmail.com>
2021-02-18 21:06:37 +0300

    add necessary folders, views, and components

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 const routes = [
   {
@@ -8,12 +11,19 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '',
+    name: 'Register',
+    component: Register
+  },
+  {
+    path: '',
+    name: 'Dashboard',
+    component: Dashboard
   }
 ]
 
